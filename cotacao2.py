ba2b4420716b9cc2eb7c0b64a50ce7632e7503cb
jumperblumenau <jumperblumenau2018@gmail.com>
2024-06-27 20:45:22 -0300

    Professor

--- cotacao2.py
+++ cotacao2.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import yfinance as yf
+from tkinter import ttk
 
 janela = tk.Tk()
 janela.title("Cotação de Moedas")
@@ -13,12 +14,15 @@ mensagem.grid(row=0, column=0, columnspan=2, sticky="NSEW")
 mensagem2 = tk.Label(text="Selecione a Moeda desejada: ")
 mensagem2.grid(row=1, column=0)
 
-moeda = tk.Entry()
-moeda.grid(row=1, column=1)
-
+# Lista de moedas
+moedas = ["USD", "CAD", "GBP"]
+moeda_selecionada = tk.StringVar()
+moeda_combobox = ttk.Combobox(janela, textvariable=moeda_selecionada, values=moedas)
+moeda_combobox.grid(row=1, column=1)
+moeda_combobox.set("Selecione uma moeda")
 
 def buscar_cotacao():
-    moeda_preenchida = moeda.get().upper()
+    moeda_preenchida = moeda_selecionada.get()
     symbol = f"{moeda_preenchida}BRL=X"
 
     try:
@@ -29,7 +33,6 @@ def buscar_cotacao():
         mensagem_cotacao = tk.Label(text="Cotação Não Encontrada ou Erro ao buscar cotação")
         mensagem_cotacao.grid(row=3, column=0, columnspan=2)
         print(f"Erro: {e}")
-
 
 botao = tk.Button(text="Buscar Cotação", command=buscar_cotacao)
 botao.grid(row=2, column=1)
